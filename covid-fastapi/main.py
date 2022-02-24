3fefa7ffa1a3b0234d4e98ca2323880ce9187ffb
sncap <sncap7@gmail.com>
2022-02-24 17:07:27 +0900

    Update main.py

--- covid-fastapi/main.py
+++ covid-fastapi/main.py
@@ -390,8 +390,9 @@ async def covid_classifier_model2(img_path, filename):
     requests.Session.trust_env = False
 
     #MODEL2_API_URL is tensorflow serving URL in another docker
-    HEADERS = {'content-type': 'application/json'}
-    MODEL2_API_URL = 'http://127.0.0.1:8511/v1/models/covid19/versions/1:predict'
+    HEADERS = {'content-type': 'application/json', 'Host': 'covid-19.myspace.example.com'}
+    #MODEL2_API_URL = 'http://127.0.0.1:8511/v1/models/covid19/versions/1:predict'
+    MODEL2_API_URL = 'http://34.64.229.247:31380/v1/models/covid-19:predict'
     CLASS_NAMES = ['Covid19', 'Normal_Lung', 'Pneumonia_Bacterial_Lung']
 
     logging.warning("****** Tenserflow Serving Request  *****")
@@ -460,8 +461,9 @@ async def covid_classifier_model2_heatmap(request: Request):
                        "instances": img.tolist()})
 
     #MODEL2_API_URL is tensorflow serving URL in another docker
-    HEADERS = {'content-type': 'application/json'}
-    MODEL2_API_URL = 'http://127.0.0.1:8511/v1/models/covid19/versions/1:predict'
+    HEADERS = {'content-type': 'application/json', 'Host': 'covid-19.myspace.example.com'}
+    #MODEL2_API_URL = 'http://127.0.0.1:8511/v1/models/covid19/versions/1:predict'
+    MODEL2_API_URL = 'http://34.64.229.247:31380/v1/models/covid-19:predict'
     CLASS_NAMES = ['Covid19', 'Normal_Lung', 'Pneumonia_Bacterial_Lung']
 
     json_response = requests.post(MODEL2_API_URL, data=data, headers=HEADERS)
